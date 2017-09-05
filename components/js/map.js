d1e27bb3c117b85819f0aef4f6d65de1ae387a18
Benjamin Wellington <benhwell@gmail.com>
2017-09-05 15:05:29 -0400

    change menu color to match title, shift title position

--- components/js/map.js
+++ components/js/map.js
@@ -3,10 +3,13 @@ const Map = ({ bounds, container }) => {
     zoomControl: false,
   }).fitBounds(bounds);
 
-  L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
-    attribution: "&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> &copy; <a href='http://cartodb.com/attributions'>CartoDB</a>",
-    subdomains: 'abcd',
-    maxZoom: 19,
+  // L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
+  //   attribution: "&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> &copy; <a href='http://cartodb.com/attributions'>CartoDB</a>",
+  //   subdomains: 'abcd',
+  //   maxZoom: 19,
+  // }).addTo(map);
+  L.tileLayer('https://api.mapbox.com/styles/v1/bwellington/citvibb7y005w2io6oa24vj7f/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYndlbGxpbmd0b24iLCJhIjoiY2loNTNjazlwMTA0ZHc5bTU3cmJ3N24zMyJ9.PRgQmgfSQma6GPYEtykZ8Q', {
+    attribution: "&copy; <a href='http://osm.org/copyright'>OpenStreetMap</a> contributors",
   }).addTo(map);
 
   return map;
